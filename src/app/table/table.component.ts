bb0651097d7e57c58126968b1ac159911c809ccc
SantiagoBursese <santi.churus@gmail.com>
2019-08-02 12:18:31 -0300

    agrego input en proyecto

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
-  vehiculos:Array<any> = [{nombre:"toyota",patente:"abc123",velocidad:"100", color: "rojo"},
+  vehiculos:Array<any> = [{nombre:"toyota",patente:"abc123",velocidad:"120", color: "rojo"},
   {nombre: "peugeot",patente:"abd123",velocidad:"85", color: "azul"},
   {nombre: "ford",patente:"bcd123",velocidad:"80", color: "verde"},
-  {nombre: "ferrari",patente:"ccc222",velocidad:"100", color: "verde"}];
+  {nombre: "ferrari",patente:"ccc222",velocidad:"100", color: "verde"},
+  {nombre: "fiat",patente:"ddd222",velocidad:"90", color: "amarillo"}];
 
   categoriaLinea(registro:any,velocidadInf:number,velocidadSup:number):String{
     if(registro.velocidad >= velocidadSup)
@@ -17,6 +18,14 @@ export class TableComponent implements OnInit {
     if(registro.velocidad >= velocidadInf && registro.velocidad <= velocidadSup)
       return 'text-warning';  
 
+  }
+
+  ocultarFilas(registro:any, minima:number):boolean{
+    return registro.velocidad > minima;
+  }
+
+  valorMinimo(valorMaximo:number):number{
+    return valorMaximo * 0.85;
   }
 
   constructor() { }
